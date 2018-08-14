0ef151c30318855dddc9ebaca2799cdf932a763c
Mathias Maciossek <mathias.maciossek@gmail.com>
2018-08-14 11:03:56 +0200

    Add better bootsrap code for assignment

--- public/script.js
+++ public/script.js
@@ -1,14 +1,21 @@
+// THIS IS FOR AUTOMATED TESTING
+if (typeof module !== 'undefined') {
+  global.$ = require('jquery')
+}
+// END
+
 $( document ).ready((() => {
   // DOMContent is laoded, now we can start checking HTML Elements
   // If we dont "wait" for document to be ready, we cannot access HTML elements
   // for testing purposes, you can use a "debugger;" statement or also "console.log(element)"
   console.log('DOM is ready!')
   
-  const input = $('#hft-shoutbox-form-input-name').get(0)
-  const textarea = $('#hft-shoutbox-form-textarea').get(0)
+  getData(); // TODO: Implement getData Method
+  const input = $('#hft-shoutbox-form-input-name')
+  const textarea = $('#hft-shoutbox-form-textarea')
 
   $('#hft-shoutbox-form').on('keyup', (event) => {
-    if (formElementIsValid(input, 3) && formElementIsValid(textarea, 10)) {
+    if (formElementIsValid(input.val(), 3) && formElementIsValid(textarea.val(), 10)) {
       toggleAlertBox(false)
       toggleSubmit(false)
     } else {
@@ -16,10 +23,12 @@ $( document ).ready((() => {
       toggleSubmit(true)
     }
   })
+
+  // TODO: Handle submit
 }))
 
 function formElementIsValid(element, minLength) {
-  return element.value.length >= minLength
+  return element.length >= minLength
 }
 
 function toggleAlertBox(show) {
@@ -36,3 +45,20 @@ function toggleSubmit(disable) {
   const submitButton = $('#hft-shoutbox-form-submit')
   submitButton.prop('disabled', disable)
 }
+
+async function getData() {
+  // TODO: Implement
+}
+
+async function saveData(username, message) {
+  // TODO: Implement
+}
+
+// THIS IS FOR AUTOMATED TESTING
+if (typeof module !== 'undefined') {
+  module.exports = {
+    getData,
+    saveData
+  }
+}
+// END
