a226f98c6cda33ad1d1b0fbb7863793d9c1e7baa
Mathias Maciossek <mathias.maciossek@gmail.com>
2018-11-29 17:40:27 +0100

    Fix Test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,9 +1,10 @@
-const Database = require('better-sqlite3')
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./db/shoutbox.db');
 const bodyParser = require('body-parser')
 const path  = require('path')
 const fs = require('fs')
 const request = require('supertest')
-const db = new Database('./db/shoutbox.db');
+
 let app
 
 describe('index.js', () => {
@@ -15,31 +16,33 @@ describe('index.js', () => {
     app.close()
   })
 
-  it('Should have the form element somewhere', (done) => {
-    fs.readFile(path.resolve('views/pages/index.ejs'), 'utf8', function (err, text) {
-      expect(text.indexOf('form') > -1).toBeTruthy()
-      done()
-    });
+  it('sqlite3 and body-parser should be installed', async () => {
+    expect(sqlite3).not.toBeUndefined()
+    expect(bodyParser).not.toBeUndefined()
   });
 
   it('Should create an GET index \'/\' route', async () => {
     await expect(request(app).get('/')).resolves.toHaveProperty('status', 200);
   });
 
-  it('Should create an GET \'/api/shouts\' route', async () => {
+  it('Should create an GET \'/add-entry\' route', async () => {
     await expect(request(app).get('/api/shouts')).resolves.toHaveProperty('status', 200);
   });
 
-  it('Should store stuff in the Database when calling POST \'/add-entry\' route', async () => {
+  it('Should store stuff in the Database when calling POST \'/add-entry\' route', async (done) => {
     await expect(request(app)
-        .post('/api/shouts')
+    .post('/api/shouts')
         .send({username: 'TESTING SCRIPT', message: 'TESTING DATA'})
-    ).resolves.toHaveProperty('status', 200);
+    ).resolves.not.toHaveProperty('status', 404);
     
 
-    const {username, message} = db.prepare('SELECT * FROM shouts WHERE username=?').get('TESTING SCRIPT');
-    expect(username).toEqual('TESTING SCRIPT');
-    expect(message).toEqual('TESTING DATA');
-    db.prepare('DELETE FROM shouts WHERE username=?').run('TESTING SCRIPT');
+    db.get('SELECT * FROM shouts WHERE username=?', ['TESTING SCRIPT'],(err, row) => {
+      expect(row.username).toEqual('TESTING SCRIPT');
+      expect(row.message).toEqual('TESTING DATA');
+
+      db.run('DELETE FROM shouts WHERE username=?', 'TESTING SCRIPT', (err) => {
+        done()
+      });
+    });
   });
 });
